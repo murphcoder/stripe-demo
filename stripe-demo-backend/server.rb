a6ca728b51dda11b74007fcb3f68ba92aaa2532e
murphcoder <murphcoder@gmail.com>
2021-10-31 23:53:16 -0500

    first draft

---
+++ stripe-demo-backend/server.rb
@@ -0,0 +1,28 @@
+require 'stripe'
+require 'sinatra'
+
+# This is your real test secret API key.
+Stripe.api_key = 'sk_test_51Jqs98IHnITDo4ldUwmD77Ryiu61jgTcTTul6JtrWZ54uIQI1EYuxiEucOR5ba2Ri7pI6EKBFLaVyHJJuGpLEhzO00IsYRS5bc'
+
+set :static, true
+set :port, 4242
+
+YOUR_DOMAIN = 'http://localhost:3000/checkout'
+
+post '/create-checkout-session' do
+  content_type 'application/json'
+
+  session = Stripe::Checkout::Session.create({
+    line_items: [{
+      price: 'price_1JqsDlIHnITDo4ldzvlf2gVP',
+      quantity: 1,
+    }],
+    payment_method_types: [
+      'card',
+    ],
+    mode: 'payment',
+    success_url: YOUR_DOMAIN + '?success=true',
+    cancel_url: YOUR_DOMAIN + '?canceled=true',
+  })
+  redirect session.url, 303
+end
--- stripe-demo-frontend/src/App.js
+++ stripe-demo-frontend/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const ProductDisplay = () => (
+  <section>
+    <div className="product">
+      <img
+        src="https://i.imgur.com/EHyR2nP.png"
+        alt="The cover of Stubborn Attachments"
+      />
+      <div className="description">
+      <h3>Pickle</h3>
+      <h5>$45.00</h5>
+      </div>
     </div>
+    <form action="http://localhost:4242/create-checkout-session" method="POST">
+      <button type="submit">
+        Checkout
+      </button>
+    </form>
+  </section>
+);
+
+const Message = ({ message }) => (
+  <section>
+    <p>{message}</p>
+  </section>
+);
+
+export default function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    // Check to see if this is a redirect back from Checkout
+    const query = new URLSearchParams(window.location.search);
+
+    if (query.get("success")) {
+      setMessage("Order placed! You will receive an email confirmation.");
+    }
+
+    if (query.get("canceled")) {
+      setMessage(
+        "Order canceled -- continue to shop around and checkout when you're ready."
+      );
+    }
+  }, []);
+
+  return message ? (
+    <Message message={message} />
+  ) : (
+    <ProductDisplay />
   );
 }
-
-export default App;
